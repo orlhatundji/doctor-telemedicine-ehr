3dfab1c916f44bf7a1a5b92ade20b6d02e7452c3
Simon Babatunde <orlhatundji@gmail.com>
2024-08-18 04:03:28 +0100

    fix backend url

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 // https://telemedicine-ehr.adaptable.app
-export const socket = io("http://localhost:3003", {
+export const socket = io("https://telemedicine-ehr.adaptable.app", {
   autoConnect: true,
 });
